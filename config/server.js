080dde673387554217d76092b2dfe10a8adf0049
portolucas <lucasportosd@gmail.com>
2021-03-15 22:20:08 -0300

    first: API with strapi haddless CMS

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7ec231d3692fd5951ff36aa1ee61a8b8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5f26b8cd-4136-478b-bd22-8f04e396db7e'
+};
